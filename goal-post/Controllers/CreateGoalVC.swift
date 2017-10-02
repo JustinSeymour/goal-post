15b7b6ad51c331f4082621085c5dcd32262b734c
Justin Seymour <justin@3wmholdings.com>
2017-10-02 11:21:18 +0200

    Lesson4 updates

---
+++ goal-post/Controllers/CreateGoalVC.swift
@@ -0,0 +1,44 @@
+//
+//  CreateGoalVC.swift
+//  goal-post
+//
+//  Created by Justin Seymour on 2017/10/02.
+//  Copyright © 2017 Justin Seymour. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalVC: UIViewController {
+
+    //Outlets
+    @IBOutlet weak var goalTextView: UITextView!
+    @IBOutlet weak var shortTermBtn: UIButton!
+    @IBOutlet weak var longTermBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    @IBAction func nextBtnWasPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func shortTermBtnWasPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func longTermBtnWasPressed(_ sender: Any) {
+        
+    }
+    
+
+    @IBAction func backBtnWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+}
