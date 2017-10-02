f9c1526bc8109f965a15230d1a03c4b2fa0a8844
Justin Seymour <justin@3wmholdings.com>
2017-10-02 12:11:36 +0200

    Lesson 6 updates

--- goal-post/Controllers/CreateGoalVC.swift
+++ goal-post/Controllers/CreateGoalVC.swift
@@ -16,10 +16,14 @@ class CreateGoalVC: UIViewController {
     @IBOutlet weak var longTermBtn: UIButton!
     @IBOutlet weak var nextBtn: UIButton!
     
+    // Variables
+    var goalType: GoalType = .shortTerm
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        nextBtn.bindToKeyboard()
+        shortTermBtn.setSelectedColor()
+        longTermBtn.setDeselectedColor()
     }
 
     @IBAction func nextBtnWasPressed(_ sender: Any) {
@@ -27,11 +31,15 @@ class CreateGoalVC: UIViewController {
     }
     
     @IBAction func shortTermBtnWasPressed(_ sender: Any) {
-        
+        goalType = .shortTerm
+        shortTermBtn.setSelectedColor()
+        longTermBtn.setDeselectedColor()
     }
     
     @IBAction func longTermBtnWasPressed(_ sender: Any) {
-        
+        goalType = .longTerm
+        longTermBtn.setSelectedColor()
+        shortTermBtn.setDeselectedColor()
     }
     
 
