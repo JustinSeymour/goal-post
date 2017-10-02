e54761da9f0751bb54eb972bcb17c73ee6370078
Justin Seymour <justin@3wmholdings.com>
2017-10-02 10:56:20 +0200

    Lesson 3 updates

---
+++ goal-post/Enums/GoalType.swift
@@ -0,0 +1,14 @@
+//
+//  GoalType.swift
+//  goal-post
+//
+//  Created by Justin Seymour on 2017/10/02.
+//  Copyright © 2017 Justin Seymour. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType: String {
+    case longTerm = "Long Term"
+    case shortTerm = "Short Term"
+}
