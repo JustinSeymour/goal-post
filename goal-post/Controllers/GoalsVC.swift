76808b1e1649030545b4561e6d0a853897995415
Justin Seymour <justin@3wmholdings.com>
2017-10-02 09:59:31 +0200

    Lesson1 changes

--- goal-post/Controllers/GoalsVC.swift
+++ goal-post/Controllers/GoalsVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  GoalsVC.swift
 //  goal-post
 //
 //  Created by Justin Seymour on 2017/10/02.
@@ -8,17 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class GoalsVC: UIViewController {
+    
+// Outlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func addGoalBtnWasPressed(_ sender: Any) {
+        
+        print("btn was pressed")
     }
+    
 
 
 }
